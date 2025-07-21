f7497a57e7eabe734c656fb18ff8a904dedfd3a8
RamiroCsatlos <ramiro.csatlos@gmail.com>
2025-07-21 20:03:06 -0300

    Correccion de errores onedrive/router

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App.jsx';
-import AboutPage from './pages/AboutPage.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
