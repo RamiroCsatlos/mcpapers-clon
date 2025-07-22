530c35eb5b479688ed42845a19b85c234a6d2f62
RamiroCsatlos <ramiro.csatlos@gmail.com>
2025-07-22 10:24:57 -0300

    Agregado la libreria de react-intersectin-observer y su uso en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import './App.css'
 import Header from './components/Header'
-import About from './components/About';
-import HeroSlider from './components/HeroSlider'
-import Products from './components/Products'
+import { Suspense, lazy } from 'react';
+import { useInView } from 'react-intersection-observer'
+
+const About = lazy(() => import('./components/About'));
+const HeroSlider = lazy(() => import('./components/HeroSlider'));
+const Products = lazy(() => import('./components/Products'));
 
 function App() {
+  const [aboutRef, aboutInView] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [sliderRef, sliderInView] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [productsRef, productsInView] = useInView({ triggerOnce: true, threshold: 0.1 });
+
   return (
     <>
       <Header />
-      <About />
-      {/* <HeroSlider />
-      <Products /> */}
+      <Suspense fallback={<div>Cargando...</div>}>
+        <div ref={aboutRef}>
+          {aboutInView && <About />}
+        </div>
+        <div ref={sliderRef}>
+          {sliderInView && <HeroSlider />}
+        </div>
+        <div ref={productsRef}>
+          {productsInView && <Products />}
+        </div>
+      </Suspense>
     </>
   )
 }
