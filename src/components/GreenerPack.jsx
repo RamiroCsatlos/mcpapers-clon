0745c909bc92204fcb3e925618a413c204f43482
RamiroCsatlos <ramiro.csatlos@gmail.com>
2025-07-25 13:52:41 -0300

    animaciones de greenerpack terminadas

--- src/components/GreenerPack.jsx
+++ src/components/GreenerPack.jsx
@@ -1,22 +1,42 @@
 import './GreenerPack.css';
+import './ScrollAnimations.css';
+import { useInView } from 'react-intersection-observer';
 import greenerImg from '../assets/greenerpackLogoColor.png';
 
-const GreenerPack = () => (
-  <section className="greenerpack-section">
-    <div className="greenerpack-bg" />
-    <div className="greenerpack-content">
-      <div className="greenerpack-img-container">
-        <a href="/">
-            <img src={greenerImg} alt="GreenerPack" className="greenerpack-img" />
-        </a>
+const GreenerPack = () => {
+  const { ref: logoRef, inView: logoInView } = useInView({
+    threshold: 0.2,
+    triggerOnce: false
+  });
+
+  const { ref: textRef, inView: textInView } = useInView({
+    threshold: 0.2,
+    triggerOnce: false
+  });
+
+  return (
+    <section className="greenerpack-section">
+      <div className="greenerpack-bg" />
+      <div className="greenerpack-content">
+        <div 
+          ref={logoRef}
+          className={`greenerpack-img-container scale-in ${logoInView ? 'fade-in-visible' : ''}`}
+        >
+          <a href="/">
+              <img src={greenerImg} alt="GreenerPack" className="greenerpack-img" />
+          </a>
+        </div>
+        <div 
+          ref={textRef}
+          className={`greenerpack-text fade-in-up animation-delay-200 ${textInView ? 'fade-in-visible' : ''}`}
+        >
+          <p>
+              Un coating interno para los wraps y flat bags que se realiza con   ceras vegetales biodegradables 100% 
+          </p>
+        </div>
       </div>
-      <div className="greenerpack-text">
-        <p>
-            Un coating interno para los wraps y flat bags que se realiza con   ceras vegetales biodegradables 100% 
-        </p>
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default GreenerPack;
